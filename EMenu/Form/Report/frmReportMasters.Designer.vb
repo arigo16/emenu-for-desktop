9da329fd3ae93043241be7881839246c81e6aaf6
DESKTOP-HFBGGH7\Arigo <arigo@edutren.com>
2018-10-09 14:34:55 +0700

    add print menus

--- EMenu/Form/Report/frmReportMasters.Designer.vb
+++ EMenu/Form/Report/frmReportMasters.Designer.vb
@@ -22,7 +22,11 @@ Partial Class frmReportMasters
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmReportMasters))
         Me.LabelX1 = New DevComponents.DotNetBar.LabelX()
+        Me.GroupPanel2 = New DevComponents.DotNetBar.Controls.GroupPanel()
+        Me.btnViewMenus = New DevComponents.DotNetBar.ButtonX()
+        Me.GroupPanel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'LabelX1
@@ -41,20 +45,82 @@ Partial Class frmReportMasters
         Me.LabelX1.Text = "Report Master"
         Me.LabelX1.TextAlignment = System.Drawing.StringAlignment.Center
         '
+        'GroupPanel2
+        '
+        Me.GroupPanel2.BackColor = System.Drawing.Color.Transparent
+        Me.GroupPanel2.CanvasColor = System.Drawing.SystemColors.Control
+        Me.GroupPanel2.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007
+        Me.GroupPanel2.Controls.Add(Me.btnViewMenus)
+        Me.GroupPanel2.Location = New System.Drawing.Point(401, 255)
+        Me.GroupPanel2.Name = "GroupPanel2"
+        Me.GroupPanel2.Size = New System.Drawing.Size(562, 137)
+        '
+        '
+        '
+        Me.GroupPanel2.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2
+        Me.GroupPanel2.Style.BackColorGradientAngle = 90
+        Me.GroupPanel2.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground
+        Me.GroupPanel2.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.GroupPanel2.Style.BorderBottomWidth = 1
+        Me.GroupPanel2.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder
+        Me.GroupPanel2.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.GroupPanel2.Style.BorderLeftWidth = 1
+        Me.GroupPanel2.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.GroupPanel2.Style.BorderRightWidth = 1
+        Me.GroupPanel2.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid
+        Me.GroupPanel2.Style.BorderTopWidth = 1
+        Me.GroupPanel2.Style.Class = ""
+        Me.GroupPanel2.Style.CornerDiameter = 4
+        Me.GroupPanel2.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded
+        Me.GroupPanel2.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center
+        Me.GroupPanel2.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText
+        Me.GroupPanel2.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near
+        '
+        '
+        '
+        Me.GroupPanel2.StyleMouseDown.Class = ""
+        Me.GroupPanel2.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        '
+        '
+        '
+        Me.GroupPanel2.StyleMouseOver.Class = ""
+        Me.GroupPanel2.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.GroupPanel2.TabIndex = 3
+        Me.GroupPanel2.Text = "Menu"
+        '
+        'btnViewMenus
+        '
+        Me.btnViewMenus.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.btnViewMenus.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.btnViewMenus.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnViewMenus.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnViewMenus.Image = CType(resources.GetObject("btnViewMenus.Image"), System.Drawing.Image)
+        Me.btnViewMenus.ImagePosition = DevComponents.DotNetBar.eImagePosition.Top
+        Me.btnViewMenus.Location = New System.Drawing.Point(230, 16)
+        Me.btnViewMenus.Name = "btnViewMenus"
+        Me.btnViewMenus.Size = New System.Drawing.Size(103, 83)
+        Me.btnViewMenus.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.btnViewMenus.TabIndex = 9
+        Me.btnViewMenus.Text = "&Print Menus"
+        '
         'frmReportMasters
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Highlight
         Me.ClientSize = New System.Drawing.Size(1360, 585)
+        Me.Controls.Add(Me.GroupPanel2)
         Me.Controls.Add(Me.LabelX1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "frmReportMasters"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Report Master"
+        Me.GroupPanel2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents LabelX1 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents GroupPanel2 As DevComponents.DotNetBar.Controls.GroupPanel
+    Friend WithEvents btnViewMenus As DevComponents.DotNetBar.ButtonX
 End Class
--- EMenu/Form/Report/frmReportMasters.vb
+++ EMenu/Form/Report/frmReportMasters.vb
@@ -1,3 +1,11 @@
 ﻿Public Class frmReportMasters
-
+    Private Sub btnViewMenus_Click(sender As Object, e As EventArgs) Handles btnViewMenus.Click
+        Dim printMenus As New printMenus
+        frmViewRpt.ViewReport.RefreshReport()
+        frmViewRpt.ViewReport.ReportSource = printMenus
+        frmViewRpt.ViewReport.RefreshReport()
+        frmViewRpt.ViewReport.Refresh()
+        frmViewRpt.Text = "List Menus"
+        frmViewRpt.Show()
+    End Sub
 End Class
